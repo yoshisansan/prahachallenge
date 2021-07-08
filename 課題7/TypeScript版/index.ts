58ac3c467e8bb6198eb389bc28a1633e8acccc26
AKIFUMI <awakewalker007@example.com>
2021-07-08 16:44:40 +0700

    リファクタリング

--- 課題7/TypeScript版/index.ts
+++ 課題7/TypeScript版/index.ts
@@ -3,5 +3,5 @@ import { resCacheServer } from "./resCacheServer";
 const clientPort = 4040;
 const resCachePort = 4000;
 
-resCacheServer(resCachePort);
 clientServer(clientPort);
+resCacheServer(resCachePort);
