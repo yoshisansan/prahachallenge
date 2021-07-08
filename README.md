7b94b5794566eb5e57ea3dc6f2175b25948d1b5e
AKIFUMI <awakewalker007@example.com>
2021-07-09 03:14:29 +0700

    calendlyハック

--- README.md
+++ README.md
@@ -2,10 +2,34 @@
 
 ## What's this tool?
 
-A tool that makes it easy to coordinate appointments in Calendly.
+直近１ヶ月の Calendly の指定した時間で予約が取れるのかどうかを確認できます。
 
-## How to
+You can check if you can make an appointment at a specified time in Calendly forthe last month.
+
+このリポジトリを参考にして作りました。  
+I created this repository as a reference.  
+https://github.com/areph/get-calendly-schedule/blob/main/app.js
+
+## How to use
+
+example
 
 ```sh
-node app.js -t <calendly_event_types> -s <start_date> -e <end_date>
+yarn start 18:00
+or
+npm start 18:00
 ```
+
+and then
+
+--∧＿∧----  
+（　 ´∀ ｀）＜　直近１ヶ月の 18:00 に空いている日程やで  
+-------つ--  
+07 月 15 日 18:00  
+07 月 16 日 18:00  
+07 月 17 日 18:00  
+07 月 22 日 18:00  
+07 月 23 日 18:00  
+.  
+.  
+.
--- app.js
+++ app.js
@@ -3,40 +3,42 @@
 const program = require('commander');
 const superagent = require('superagent');
 const dayjs = require('dayjs');
+const uuid = 'GFFTHFYOBDB57WUG';
 
 program
   .version('0.0.1')
-  .requiredOption('-t, --event_type <type>', 'set calendly event_type')
-  .requiredOption('-s, --start_date <type>', 'set startdate ex) 2021-07-11')
-  .requiredOption('-e, --end_date <type>', 'set enddate ex) 2021-07-12')
+  .requiredOption('-d, --date_time <type>', 'set calendly event_type')
   .parse(process.argv);
 
 const options = program.opts();
-if (!options.event_type || !options.start_date || !options.end_date) {
+if (!options.date_time) {
+  console.log('オプションを指定してください。ex: node app.js -d 18:00');
+
   return;
 }
 
-const DAY_OF_WEEK = ['日', '月', '火', '水', '木', '金', '土'];
 (async () => {
   try {
-    const url = `https://calendly.com/api/booking/event_types/${options.event_type}/calendar/range`;
+    const url = `https://calendly.com/api/booking/event_types/${uuid}/calendar/range`;
     const query = {
       timezone: 'Asia/Tokyo',
       diagnostics: false,
-      range_start: options.start_date,
-      range_end: options.end_date,
+      range_start: dayjs(new Date()).format('YYYY-MM-DD'),
+      range_end: dayjs(new Date()).add(1, 'month').format('YYYY-MM-DD'),
     };
     const response = await superagent.get(url).query(query);
     const days = JSON.parse(response.text)['days'];
+
+    console.log(
+      ' --∧＿∧----\n', // eslint-disable-next-line no-irregular-whitespace
+      `（　´∀｀）＜　直近１ヶ月の${options.date_time}に空いている日程やで\n`,
+      '-------つ--'
+    );
     for (const day of days) {
-      console.log(
-        '----',
-        `${day['date']}(${DAY_OF_WEEK[dayjs(day['date']).day()]})`,
-        '----'
-      );
       day['spots']
         .flatMap((x) => x['start_time'])
-        .map((x) => console.log(dayjs(x).format('HH:mm')));
+        .filter((x) => options.date_time === dayjs(x).format('HH:mm'))
+        .map((x) => console.log(dayjs(x).format('MM月DD日 HH:mm')));
     }
   } catch (error) {
     console.log(error.response.body);
